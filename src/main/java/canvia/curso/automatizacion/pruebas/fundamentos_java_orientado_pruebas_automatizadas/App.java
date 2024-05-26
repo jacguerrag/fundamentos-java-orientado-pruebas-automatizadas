7fce44d1218375500e9a77359813755a51daeabd
jacguerrag <jacguerrag@gmail.com>
2024-05-26 14:57:34 -0600

    proyecto basico

---
+++ src/main/java/canvia/curso/automatizacion/pruebas/fundamentos_java_orientado_pruebas_automatizadas/App.java
@@ -0,0 +1,13 @@
+package canvia.curso.automatizacion.pruebas.fundamentos_java_orientado_pruebas_automatizadas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
