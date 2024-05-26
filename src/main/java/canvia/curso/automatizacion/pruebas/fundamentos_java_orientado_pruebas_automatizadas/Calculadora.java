3f5c5f34ca95fee5471f9937b7fcd7bcbc707a75
jacguerrag <jacguerrag@gmail.com>
2024-05-26 15:28:55 -0600

    definicion funcion para prueba

--- src/main/java/canvia/curso/automatizacion/pruebas/fundamentos_java_orientado_pruebas_automatizadas/Calculadora.java
+++ src/main/java/canvia/curso/automatizacion/pruebas/fundamentos_java_orientado_pruebas_automatizadas/Calculadora.java
@@ -1,6 +1,6 @@
 package canvia.curso.automatizacion.pruebas.fundamentos_java_orientado_pruebas_automatizadas;
 
-public class Calculator {
+public class Calculadora {
     public int sum(int a, int b) {
         return a + b;
     }
